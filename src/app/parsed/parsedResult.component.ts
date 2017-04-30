ea668ed53fc1477379177249b9a1817d8721d0a8
Eugene Belov <eugene.belov@smashdocs.net>
2017-04-30 16:43:50 +0300

    add parsed view componennt

---
+++ src/app/parsed/parsedResult.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'parsed-result',
+  templateUrl: './parsedResult.component.html'
+})
+export class ParsedResult {
+  titleSource = "Source";
+  titleTarget = "Target";
+}

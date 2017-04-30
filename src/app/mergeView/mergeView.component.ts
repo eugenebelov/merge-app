6e117897433c16caf0c3e17751eac44684c13671
Eugene Belov <eugene.belov@smashdocs.net>
2017-04-30 15:24:05 +0300

    add styles and update merge componentn

---
+++ src/app/mergeView/mergeView.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'merge-view',
+  templateUrl: './mergeView.component.html'
+})
+export class MergeView {
+  titleSource = "Source";
+  titleTarget = "Target";
+}

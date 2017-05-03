137d424ec2bf2b61de9aed88d1674cf5441093c6
Eugene Belov <eugene.belov@smashdocs.net>
2017-05-03 20:26:29 +0300

    get result and build

--- src/app/parsed/parsedResult.component.ts
+++ src/app/parsed/parsedResult.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+
+import { DiffService } from '../diffService/diff.service';
 
 @Component({
   selector: 'parsed-result',
@@ -7,4 +10,24 @@ import { Component } from '@angular/core';
 export class ParsedResult {
   titleSource = "Source";
   titleTarget = "Target";
+
+  private subscribeToDiff: Subscription;
+
+  constructor(private diffService: DiffService) {}
+
+  ngOnInit() {
+    this.subscribeToDiff = this.diffService.notifyObservable$.subscribe((res) => {
+      if (res.hasOwnProperty('difference')) {
+        this.buildResult(res.difference);
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscribeToDiff.unsubscribe();
+  }
+
+  private buildResult(diff:Array<any>) {
+    console.debug('buildResult', diff);
+  }
 }
